a41e8cc2b0d03d7e9ce39d7134b1b196cf8533f8
Dilshaad21 <dilshaadmuthalif21@gmail.com>
2021-07-18 15:13:14 +0530

    Transfer tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,12 +1,34 @@
 pragma solidity >=0.4.2;
 
 contract DappToken{
-    // Constructor
-    // Set the total number of tokens
-    // Read the total number of tokens
+
+    string public name = "Dapp Token";
+    string public symbol = "DAPP";
+    string public standard = "Dapp Token v1.0";
     uint256 public totalSupply;
 
-    constructor() public{
-        totalSupply = 1000000;
+    event Transfer(
+        address indexed _from,  
+        address indexed _to, 
+        uint256 _value
+    );
+    
+    mapping(address => uint256) public balanceOf;
+
+    constructor(uint256 _initialSupply) public{
+        // First account which deplyed contract
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+        // allocate the initial supply 
+    }
+    
+    function transfer(address _to, uint256 _value) public returns (bool success){
+        require (balanceOf[msg.sender] >= _value);
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
